acbda591d310957aaaf0ec9e0f4b95ae9171d1e8
version-1 <j.onodera26@gmail.com>
2020-06-10 10:19:44 -0700

    add AddingQuestion class

---
+++ Lab3/AddingQuestion.h
@@ -0,0 +1,19 @@
+//
+//  AddingQuestion.h
+//  ObjectiveC-Labs
+//
+//  Created by Administlator on 2020/06/10.
+//  Copyright © 2020 Administlator. All rights reserved.
+//
+
+#ifndef AddingQuestion_h
+#define AddingQuestion_h
+
+@interface AddingQuestion: NSObject
+{
+}
+- (int) generateRandomQuestion;
+@end
+
+
+#endif /* AddingQuestion_h */
---
+++ Lab3/AddingQuestion.m
@@ -0,0 +1,10 @@
+//
+//  AddingQuestion.m
+//  Lab3
+//
+//  Created by Administlator on 2020/06/10.
+//  Copyright © 2020 Administlator. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AddingQuestion.h"
