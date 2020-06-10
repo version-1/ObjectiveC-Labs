91af0b83bdea3ebe1c9a49af1811401d6acedd7b
version-1 <j.onodera26@gmail.com>
2020-06-10 07:18:19 -0700

    Initial Commit

---
+++ ObjectiveC-Labs/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectiveC-Labs
+//
+//  Created by Administlator on 2020/06/10.
+//  Copyright © 2020 Administlator. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
